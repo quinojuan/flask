8615bbd12d9a8af6ae7df6fb38df73eb46570757
Juan Quino <quinojuan@gmail.com>
2025-02-01 23:23:28 -0300

    documentation el objeto request

--- hello.py
+++ hello.py
@@ -1,23 +1,62 @@
 from flask import Flask
-from markupsafe import escape
+from flask import render_template
+# from flask import request
+# from markupsafe import escape
+# from flask import url_for
 
 app = Flask(__name__)
 
-@app.route("/user/<username>")
-def show_user_profile(username):
-  # show the user profile for that user
-  return f"User {escape(username)}"
+# @app.route("/user/<username>")
+# def show_user_profile(username):
+#   # show the user profile for that user
+#   return f"User {escape(username)}"
 
-@app.route("/hola/<name>")
-def hello(name):
-  return f"Hola {name}"
+# @app.route("/hola/<name>")
+# def hello(name):
+#   return f"Hola {name}"
 
-@app.route("/post/<int:post_id>")
-def show_post(post_id):
-  # show the post with the given id, the id is integer
-  return f"Post {post_id}"
+# @app.route("/post/<int:post_id>")
+# def show_post(post_id):
+#   # show the post with the given id, the id is integer
+#   return f"Post {post_id}"
 
-@app.route("/path/<path:subpath>")
-def show_subpath(subpath):
-  # show the subpath after /path/
-  return f"Subpath {escape(subpath)}"
+# @app.route("/path/<path:subpath>")
+# def show_subpath(subpath):
+#   # show the subpath after /path/
+#   return f"Subpath {escape(subpath)}"
+
+# @app.route("/projects/")
+# def projects():
+#   return "The project page"
+
+# @app.route("/about")
+# def about():
+#   return "The about page"
+
+# @app.route('/')
+# def index():
+#     return 'index'
+
+# @app.route('/login')
+# def login():
+#     return 'login'
+
+# @app.route('/user/<username>')
+# def profile(username):
+#     return f'{username}\'s profile'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+
+
+#################################################
+# @app.route('/login', methods=['GET', 'POST']) #
+#################################################
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+  return render_template("hello.html", person=name)
